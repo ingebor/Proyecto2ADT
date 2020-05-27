3b32e7226c0c8b82238568ba35c5f2df25e23574
Say19705 <54776073+Say19705@users.noreply.github.com>
2020-05-27 17:58:55 -0600

    Update ConexionBD.py

--- Python/ConexionBD.py
+++ Python/ConexionBD.py
@@ -4,7 +4,7 @@ graph = Graph("http://neo4j:Andres9740@127.0.0.1:7474/db/data")
 #llenar un diccionario de enfermedades
 def Enfdict():
     dit = {}
-    enf = graph.run("match (a:Enfermedad) Return a.name, a.Diarrea, a.DolorCabeza,a.DolorEstomago,a.Estornudo,a.DolorGeneral,a.FaltaEnergia").data()
+    enf = graph.run("match (a:Enfermedad) Return a.name, a.Diarrea, a.DolorCabeza,a.DolorEstomago,a.Estornudo,a.DolorGeneral,a.FaltaEnergia,a.Tos,a.Vomito").data()
     for lista in enf:
         lista1 = []
         d = lista["a.Diarrea"]
@@ -13,12 +13,16 @@ def Enfdict():
         e = lista["a.Estornudo"]
         dg = lista["a.DolorGeneral"]
         fe = lista["a.FaltaEnergia"]
+        t = lista["a.Tos"]
+        v = lista["a.Vomito"]
         lista1.append(d)
         lista1.append(dc)
         lista1.append(de)
         lista1.append(e)
         lista1.append(dg)
         lista1.append(fe)
+        lista1.append(t)
+        lista1.append(v)
         dit[lista["a.name"]] = lista1
     return dit
         
@@ -43,7 +47,7 @@ def Meddict():
 #llenar una lista de enfermedades
 def listEnf():
     listaFinal = []
-    enf = graph.run("match (a:Enfermedad) Return a.name, a.Diarrea, a.DolorCabeza,a.DolorEstomago,a.Estornudo,a.DolorGeneral,a.FaltaEnergia").data()
+    enf = graph.run("match (a:Enfermedad) Return a.name, a.Diarrea, a.DolorCabeza,a.DolorEstomago,a.Estornudo,a.DolorGeneral,a.FaltaEnergia,a.Tos,a.Vomito").data()
     for lista in enf:
         listaTemp = []
         listaTemp.append(lista["a.name"])
@@ -53,6 +57,8 @@ def listEnf():
         listaTemp.append(lista["a.Estornudo"])
         listaTemp.append(lista["a.DolorGeneral"])
         listaTemp.append(lista["a.FaltaEnergia"])
+        listaTemp.append(lista["a.Tos"])
+        listaTemp.append(lista["a.Vomito"])
         listaFinal.append(listaTemp)
     return listaFinal
 
@@ -90,4 +96,33 @@ def eliminarMed(name):
 def eliminarEnf(name):
     graph.run("match (n:Enfermedad {name:"+name+"}) delete n")
 #recomendacion
+#Diarrea,DolorCabeza,DolorEstomago,Estornudo,DolorGeneral,FaltaEnergia,Tos,Vomito,
+def Recomendacion(Diarrea,DolorCabeza,DolorEstomago,Estornudo,DolorGeneral,FaltaEnergia,Tos,Vomito,listaEnf):
+    mensaje = "puede tener:"
+    for lista in listaEnf:
+        contador = 0
+        if Diarrea == lista[1]:
+            contador += 1
+        if DolorCabeza == lista[2]:
+            contador += 1
+        if DolorEstomago == lista[3]:
+            contador += 1
+        if Estornudo == lista[4]:
+            contador += 1
+        if DolorGeneral == lista[5]:
+            contador += 1
+        if FaltaEnergia == lista[6]:
+            contador += 1
+        if Tos == lista[7]:
+            contador += 1
+        if Vomito == lista[8]:
+            contador += 1
+        if contador >= 3:
+            prob = ((contador/8)*100)-1
+            probF = str(prob)
+            mensaje += "\n" + lista[0] + " Con una Probabilidad de " + probF + "%"
+    if mensaje == "puede tener:":
+        mensaje = "No se ha encontrado ninguna enfermedad con este patron"
+    return mensaje
+
     
